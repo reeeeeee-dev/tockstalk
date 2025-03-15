a824c68a1d7f8732fc1a50bc5c1210c096633885
Reetik Patel <reetikp@gmail.com>
2025-03-14 21:39:28 -0500

    🔧 setting up video recording

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "16ped7",
   chromeWebSecurity: true,
   viewportWidth: 1024,
   viewportHeight: 2048,
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {},
   },
